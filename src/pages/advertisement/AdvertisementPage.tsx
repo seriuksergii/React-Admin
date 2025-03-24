4d42d27222866d0cd40dbb8dea06b59efd5b06a9
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2025-03-24 14:43:33 +0200

    fix

--- src/pages/advertisement/AdvertisementPage.tsx
+++ src/pages/advertisement/AdvertisementPage.tsx
@@ -1,45 +1,14 @@
-import { Box, Typography } from "@mui/material";
-import BookSearchList from "./BookSearchList";
-import AddToTopDialog from "./AddToTopDialog";
-import TopBooksList from "./TopBooksList";
-import { useState } from "react";
-
-interface Book {
-  id: string;
-  name: string;
-}
+import { TopBooksList, BookSearch } from "./TopBookList";
 
 const AdvertisementPage = () => {
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const handleOpenDialog = (book: Book) => {
-    setSelectedBook(book);
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
-
   return (
-    <Box>
-      <Typography variant="h4" gutterBottom>
-        Реклама
-      </Typography>
-
-      <BookSearchList onSelectBook={handleOpenDialog} />
-
-      {openDialog && (
-        <AddToTopDialog book={selectedBook} onClose={handleCloseDialog} />
-      )}
-
-      {/* Список книжок у топі */}
-      <Typography variant="h5" gutterBottom style={{ marginTop: "20px" }}>
-        Книжки у топі
-      </Typography>
+    <div>
+      <h1>Реклама</h1>
+      <h2>Вивести в топ</h2>
+      <BookSearch />
+      <h2>Книжки в топі</h2>
       <TopBooksList />
-    </Box>
+    </div>
   );
 };
 
